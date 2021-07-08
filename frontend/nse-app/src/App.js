9300b3549eec7a905169ad4eeaa67e3955bb48b2
Lei Wang <wanglei.okay@gmail.com>
2021-07-08 14:17:48 +0300

    chore: update code

--- frontend/nse-app/src/App.js
+++ frontend/nse-app/src/App.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pet from "./Pet";
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//   ]);
-// };
+import Search from "./Search";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+      <h1>Neural Search</h1>
+      <Search />
     </div>
     )
 }
