9dbabe817e0912dba7ff201ac2b3f5585891f53b
Lei Wang <wanglei.okay@gmail.com>
2021-07-14 00:22:45 +0300

    chore: update code

--- ai-search/src/SearchParams.js
+++ ai-search/src/SearchParams.js
@@ -1,19 +1,22 @@
 import { useState } from 'react';
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           location
-          <input id="location" value={location.toUpperCase()}
-           placeholder="Location" />
+          <input id="location"
+            onChange={(e) => setLocation(e.target.value)} 
+            value={location}
+            placeholder="Location" />
         </label>
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
