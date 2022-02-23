08801b13eb30aa9fedbdd3fa010b84627193b7fd
Lei Wang <wanglei.okay@gmail.com>
2022-02-23 10:17:51 +0000

    chore: update tasks

--- celery_task_faiss/tasks.py
+++ celery_task_faiss/tasks.py
@@ -3,9 +3,21 @@ This tasks.py now is for exploring the Celery, later we need to
 re-define it
 """
 from __future__ import absolute_import, unicode_literals
+import faiss
+import numpy as np
+
 from .worker import app
 
 
 @app.task
-def add(x, y):
-    return x + y
+def faissIndexing(vector):
+    """
+    TBD
+    """
+    if not vector:
+        embedding = np.array(vector)
+    else:
+        raise ValueError(
+            f"ValueError exception thrown, because input feature vecgtor: {vector}"
+        )
+
