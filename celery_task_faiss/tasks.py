bb0af55486edf5d9f8b5558f40c395de91154d54
Lei Wang <wanglei.okay@gmail.com>
2022-02-24 12:11:27 +0000

    chore

--- celery_task_faiss/tasks.py
+++ celery_task_faiss/tasks.py
@@ -10,14 +10,34 @@ from .worker import app
 
 
 @app.task
-def faissIndexing(vector):
+def faissIndexing(vector, embeddings_path="./embeddings", top_k=42):
     """
     TBD
     """
     if not vector:
-        embedding = np.array(vector)
+        feature_vector = np.array(vector)
     else:
         raise ValueError(
             f"ValueError exception thrown, because input feature vecgtor: {vector}"
         )
 
+    embeddings = np.load(embeddings_path)
+    _, d = embeddings.shape
+
+    # Build the index
+    index = faiss.IndexFlatL2(d)
+    index.add(embeddings)
+
+    _, topk_indexes = index.search(img_embeddings, top_k)
+    topk_list = list(topk_indexes[0])
+
+    result = []
+    for i in topk_list:
+        result.append({
+            "index": topk_list.index(i),
+            "img": f'https://storage.googleapis.com/neural-search-prod/{i}.jpg',
+            "title": f'Image Index {i}',
+            "desc": "Demo Image"
+            })
+
+    return json.dumps(result)
--- celery_task_faiss/worker.py
+++ celery_task_faiss/worker.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-BROKER_URI = os.environ.get('BROKER_URI', 'redis://localhost:6379/8')
-BACKEND_URI = os.environ.get('BACKEND_URI', 'redis://localhost:6379/8')
+BROKER_URI = os.environ.get('BROKER_URI', 'redis://localhost:6379/9')
+BACKEND_URI = os.environ.get('BACKEND_URI', 'redis://localhost:6379/9')
 
 app = Celery(
     'celery_faiss_app',
